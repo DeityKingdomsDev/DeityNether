1b2fedc1db0247e4dfcee0fc9a3ae907b4a295b4
cliff777 <bugman12345@gmail.com>
2012-06-12 19:32:56 -0400

    Updating main class

--- com/imdeity/DeityKingdomsDev/DeityNether/DeityNether.java
+++ com/imdeity/DeityKingdomsDev/DeityNether/DeityNether.java
@@ -1,12 +1,10 @@
 package com.imdeity.DeityKingdomsDev.DeityNether;
 
-
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 import com.imdeity.DeityKingdomsDev.DeityNether.listeners.PigmanListener;
 
 public class DeityNether extends JavaPlugin {
+	public static int PIGMAN_DROP_GOLD_CHANCE = 10; //Chance for pigman to drop gold - 10 = 10% chance
 	
 	public void onEnable(){
 		this.getCommand("nether").setExecutor(new NetherCommand(this));
