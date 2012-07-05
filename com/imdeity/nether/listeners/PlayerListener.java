114a927a46945bc95e5e4e7396f20e6a37f4df04
mbonachea <bonacheam@gmail.com>
2012-07-05 01:46:03 -0400

    added PlayerListener for QuitEvent

---
+++ com/imdeity/nether/listeners/PlayerListener.java
@@ -0,0 +1,26 @@
+package com.imdeity.nether.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.imdeity.nether.DeityNether;
+import com.imdeity.nether.sql.NetherSQL;
+
+public class PlayerListener implements Listener {
+	private final DeityNether plugin;
+	Player player;
+	
+	public PlayerListener(DeityNether plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		if(player.getWorld().getName().equals("world_nether")) {
+			NetherSQL.removePlayer(player);
+			player.teleport(plugin.getServer().getWorld("world").getSpawnLocation());
+		}
+	}
+}
