d1cfd3297b7975f82bb2b44ee806fd2ebd2f0944
mbonachea <bonacheam@gmail.com>
2012-07-31 11:38:34 -0400

    removed unnecessary classes

--- src/com/imdeity/nether/cmds/CmdHandler.java
+++ src/com/imdeity/nether/cmds/CmdHandler.java
@@ -13,8 +13,6 @@ public class CmdHandler extends DeityCommandHandler {
 		this.registerCommand("join", "", "Teleports the user to the nether", new JoinSubCommand(), "Deity.nether.general");
 		this.registerCommand("leave", "", "Teleports the user out of the nether and back to the main world", new LeaveSubCommand(), "Deity.nether.general");
 		this.registerCommand("regen", "", "Regenerates the nether", new RegenSubCommand(), "Deity.nether.regen");
-		this.registerCommand("help", "", "Lists all available commands to the user", new HelpSubCommand(), "Deity.nether.general");
-		this.registerCommand("?", "", "Lists all available commands to the user", new HelpSubCommand(), "Deity.nether.general");
 		this.registerCommand("time", "", "Tells the user the amount of time they have left in the nether", new TimeSubCommand(), "Deity.nether.general");
 	}
 }
